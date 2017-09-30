81691141754461e6d6bc8c0b8aee634eda8285d5
Lialosiu <lialosiu@gmail.com>
2017-09-30 15:55:31 +0800

    update

--- themes/hukami/source/js/app.js
+++ themes/hukami/source/js/app.js
@@ -4,6 +4,25 @@ let app = new Vue({
         return {
             nowDatetime: moment().format('YYYY-MM-DD HH:mm:ss')
         }
+    },
+    mounted: function () {
+        "use strict";
+        $('.ui.sticky').each((key, item) => {
+            if ($(item).hasClass('article-header')) {
+                $(item).sticky({
+                    context: $(item).parents('.article'),
+                    offset: 50,
+                    bottomOffset: 50,
+                })
+            }
+            if ($(item).hasClass('article-meta')) {
+                $(item).sticky({
+                    context: $(item).parents('.article'),
+                    offset: 170,
+                    bottomOffset: 160,
+                })
+            }
+        });
     }
 });
 
